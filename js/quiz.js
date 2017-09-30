59604d24a72b81da11d17ff452efad7474c6989a
杉山希望 <nontan4061@gmail.com>
2017-10-01 03:05:50 +0900

    initial commit

---
+++ js/quiz.js
@@ -0,0 +1,123 @@
+//コメントアウトされているのはボツにしたものです
+
+
+var queList = ['問題1:テトリスを作ったのは日本人である',
+               '問題2:１円玉の直径は2cmである',
+               '問題3:塩はカロリー0である',
+               '問題4:レモンはミカン科の果物である',
+               '問題5:パンとご飯を同じ分量だけ食べた時、消化が早いのはパンである',	//問題のリスト
+               '問題６:ドライブスルーは馬で入っても注文できる'];
+var ansNum = [0, 1, 1, 1, 0, 1];   //0の時NOを選ぶと正解、1の時YESを選ぶと正解
+var x = 6;//問題数
+var i = 0;  //正解数
+var count = 1;  //現在の回答Nom.
+var h1id = 'h1id';
+
+//ここにstartClick
+function startClick(){
+    location.href = "quiz-question.html";
+}
+
+function backColor(){
+  document.body.style.backgroundColor = "lightgreen";
+}
+
+function question() {
+    var h1Text = document.getElementById(h1id);
+    h1Text.textContent = queList[0];
+    document.body.style.backgroundColor = "lightgreen";
+}
+
+function onClickChoiceYes() {
+    var h1Text = document.getElementById(h1id);
+    h1Text.textContent = queList[count];
+
+    if (ansNum[count - 1] === 0) {
+        question_judge(false);
+    } else {
+        i = i + 1;
+        question_judge(true);
+    }
+}
+
+function onClickChoiceNo() {
+    var h1Text = document.getElementById(h1id);
+    h1Text.textContent = queList[count];
+
+    if (ansNum[count - 1] === 0) {
+        i = i + 1;
+        question_judge(true);
+    } else {
+        question_judge(false);
+    }
+}
+
+function question_judge(check) {
+    if (check === true) {
+        swal("正解", count + "問目終了。", "success");
+    } else {
+        swal("不正解", count + "問目終了。", "error");
+    }
+//ここにif文
+	if(count < x){
+		countUp();
+	}else{
+		location.href='quiz-result.html';
+		//まほうのことば
+		localStorage.setItem('access_count',i);
+	}
+
+}
+
+function countUp() {
+    count = count + 1;
+    return count;
+}
+
+function kekka() {
+    var j = localStorage.getItem('access_count'); //正解数
+    var k = x - j;      //不正解数(正解数ー問題数)
+    var m = float(j / x,2);     //正答率を求める
+    document.getElementById("seikai").innerHTML = j;
+    document.getElementById("huseikai").innerHTML = k;
+    document.getElementById("seitouritu").innerHTML = m * 100 + "%"
+}
+
+function float(number, n) {
+    var _pow = Math.pow( 10 , n ) ;
+
+    return Math.round( number * _pow ) / _pow ;
+}
+
+/*
+ function onClickTrue() {
+ var h1text = document.getElementById("h1id");
+ h1text.textContent = queList[count];
+ document.getElementById("buttonA").value=ansList[count];
+ document.getElementById("buttonB").value=ansList[count+5];
+ i = i + 1;
+ alert("正解！");
+ }
+
+ function onClickFalse() {
+ var h1text = document.getElementById("h1id");
+ h1text.textContent = queList[count];
+ document.getElementById("buttonA").value=ansList[count];
+ document.getElementById("buttonB").value=ansList[count+5];
+ alert("不正解・・・");
+ }
+
+
+
+ function counterA()   {
+ document.getElementsById("buttonA").onclick = function() {
+ countUp();
+ }
+ }
+
+ function counterB(){
+ document.getElementsById("buttonB").onclick = function(){
+ countUp();
+ }
+ }
+ */
