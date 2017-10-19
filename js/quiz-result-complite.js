82b6ae06744c73ce892664cfec2dd1aff75a0def
杉山希望 <nontan4061@gmail.com>
2017-10-19 19:42:42 +0900

    fix all file

---
+++ js/quiz-result-complite.js
@@ -0,0 +1,9 @@
+function result() {
+    var score = localStorage.getItem("score");
+    var quizCount = localStorage.getItem("quizCount");
+    var missCount = quizCount - score;
+    var percentage = Math.round(score/quizCount * 100);
+    document.getElementById("score").innerText = score;
+    document.getElementById("missCount").innerText = missCount;
+    document.getElementById("percentage").innerText = percentage + "%"
+}
